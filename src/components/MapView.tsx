6c6635678b969223c3cfeee6f816171bb214aac3
Migue Martelo <miguemartelo1031@gmail.com>
2022-07-01 19:48:22 -0500

    feat: show mapBox map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,12 +1,40 @@
-import { useContext } from 'react';
+import { useContext, useLayoutEffect, useRef } from 'react';
+import { Map } from 'mapbox-gl';
+
 import { PlacesContext } from '../context';
 import { Loading } from '.';
 
 export const MapView = () => {
   const { isLoading, userLocation } = useContext(PlacesContext);
+  const mapDiv = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      const map = new Map({
+        container: mapDiv.current!, // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: userLocation, // starting position [lng, lat]
+        zoom: 14, // starting zoomisplay the map as a 3D globe
+      });
+    }
+  }, [isLoading]);
 
   if (isLoading) {
     return <Loading />;
   }
-  return <div>{userLocation?.join(',')}</div>;
+
+  return (
+    <div
+      ref={mapDiv}
+      style={{
+        height: '100vh',
+        left: 0,
+        position: 'fixed',
+        top: 0,
+        width: '100vw',
+      }}
+    >
+      {userLocation?.join(',')}
+    </div>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
 
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWlndWVtYXJ0ZWxvIiwiYSI6ImNsNTM1anhuMDBqankzam9jdDV3ZDIxZDEifQ.sxRBS0RsklrAGBoFV8mklQ';
+
 if (!navigator.geolocation) {
   alert('Geolocation is not supported by your browser');
   throw new Error('Geolocation is not supported by your browser');
