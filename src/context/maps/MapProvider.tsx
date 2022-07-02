6b477907886b43259dddc07b76e4e5b388e50bbe
Migue Martelo <miguemartelo1031@gmail.com>
2022-07-01 22:30:28 -0500

    feat: add polyline to see the route between places

--- src/context/maps/MapProvider.tsx
+++ src/context/maps/MapProvider.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useReducer } from 'react';
-import { Map, Marker, Popup } from 'mapbox-gl';
+import { AnySourceData, LngLatBounds, Map, Marker, Popup } from 'mapbox-gl';
 import { MapContext } from './MapContext';
 import { mapReducer } from './mapReducer';
 import { PlacesContext } from '..';
@@ -64,8 +64,62 @@ export const MapProvider = ({ children }: Props) => {
     const resp = await directionsApi.get<DirectionsResponse>(
       `/${start.join(',')};${end.join(',')}`
     );
+    const { distance, duration, geometry } = resp.data.routes[0];
+    const { coordinates: coords } = geometry;
 
-    console.log(resp);
+    let kms = distance / 1000;
+    kms = Math.round(kms * 100) / 100;
+
+    const minutes = Math.round(duration / 60);
+    console.log({ kms, minutes });
+
+    const bounds = new LngLatBounds(start, start);
+
+    for (const coord of coords) {
+      const newCoord: [number, number] = [coord[0], coord[1]];
+      bounds.extend(newCoord);
+    }
+
+    state.map?.fitBounds(bounds, { padding: 200 });
+
+    //Polyline
+    const sourceData: AnySourceData = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coords,
+            },
+          },
+        ],
+      },
+    };
+
+    if (state.map?.getLayer('RouteString')) {
+      state.map.removeLayer('RouteString');
+      state.map.removeSource('RouteString');
+    }
+
+    state.map?.addSource('RouteString', sourceData);
+
+    state.map?.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round',
+      },
+      paint: {
+        'line-color': '#18323a',
+        'line-width': 5,
+      },
+    });
   };
 
   return (
